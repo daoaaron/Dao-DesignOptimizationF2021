dc9f156ba88a0d1ab3a51d6a882591949ebc0fa9
daoaaron <89168728+daoaaron@users.noreply.github.com>
2021-11-24 11:03:20 -0700

    Update HW5_Draft.py

--- HW5/HW5_Draft.py
+++ HW5/HW5_Draft.py
@@ -20,7 +20,7 @@ def g1(x):
 def g2(x):
     return (x[1]-1)**2 + 5*x[0] - 15
 
-def dLdx(x, mu):
+def Dldx(x, mu):
     mu1=mu[0]
     mu2=mu[1]
     x1=x[0]
@@ -29,17 +29,37 @@ def dLdx(x, mu):
     bottom=2*(mu1+mu2+1)*x2 - 2*(mu2+3)
     return np.array([[top],[bottom]])
 
-def QP():
+#%%
+
+def QP(x,W,A):
+    # Need to output [s lam_bar]'
+    
+    # 1.  Active constraints
+    
+    
+  #  if  # regarding mu > 0 
+    
+    
+   # if # regarding dgdx*s + g <= 0 
+    
+    return
+
+def merit(x,mu):
+    w1=
+    objf(x) + w1*max(0,g1(x)) + w2*max(0,g2(x))
     return
 
 def linesearch():
+    a=1
+    t=0.5
+    
     return
 
 def BFGS():
     return
 
 
-#%%
+#%% Get ready to run 
 W0=np.identity(2)
 W_store=[W0]
 mu=np.array([0,0])
@@ -48,10 +68,40 @@ e=1e-3
 
 x0=np.array([1,1])
 x_store=[x0]
+
+grads=np.array([])  # Storing g. Is this the 
+norm=np.linalg.norm(Dldx(x0,mu))  # Initialize.
+norms=[norm]
 #%%
 
-while (np.linalg.norm(dLdx(x_store[-1],mu)))>e:
+while norm >e:
     x=x_store[-1]
+   
+    
+    
+    # 3.1. Solve QP for sk, mu_k+1
+    
+    # 3.2. Perform a line search. Take in mu_k+1, sk. 
+        # Update weights in merit function w based on mu_k+1.
+    
+    # 3.3 calcualte x_k+1.
+    
+    
+    
+    # 3.4 Calculate W_k+1 with BFGS.
+        # 3.4.1 Evaluate dLdx for x_k, mu_k+1
+        # 3.4.2 Evaluate dLdx for x_k+1, mu_k+1
+        # 3.4.3 Evaluate theta
+        # 3.4.4 Evaluate y
+        # 3.4.5 Perform BFGS with y, sk, Wk
+    
+    # Calculate the gradient. 
+    
+    # find the norm of the gradient.
+
+#%%
+
+    
     
     
     
